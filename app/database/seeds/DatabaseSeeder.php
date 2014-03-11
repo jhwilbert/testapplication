9781f51d77a8d7420e88e5a55008727a2c73ff73
Victor <victorbv@gmail.com>
2014-03-10 21:42:29 -0300

    User model

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -7,11 +7,25 @@ class DatabaseSeeder extends Seeder {
 	 *
 	 * @return void
 	 */
-	public function run()
-	{
+	public function run() {
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('UserTableSeeder');
 	}
 
+}
+
+class UserTableSeeder extends Seeder {
+
+	public function run(){ 
+
+		DB::table('users')->delete();
+
+		User::create(array(
+			'name' => 'Victor',
+			'email' => 'victor@lapisraro.com.br',
+			'password' => Hash::make('123456')
+		));
+
+	}
 }
