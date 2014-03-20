9cb7352a15f8a68833a9dd0121e840ed58ae390c
Victor Volker <victor@lapisraro.com.br>
2014-03-19 21:15:37 -0300

    Boilerplate

--- app/views/shared/_footer_scripts.blade.php
+++ app/views/shared/_footer_scripts.blade.php
@@ -1,3 +1,17 @@
 
+{{ HTML::script('public/js/vendor/jquery-1.10.2.min.js'); }}
 {{ HTML::script('public/js/vendor/skrollr.min.js'); }}
+{{ HTML::script('public/js/plugins.js'); }}
 {{ HTML::script('public/js/application.js'); }}
+
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<script>
+    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+    e.src='//www.google-analytics.com/analytics.js';
+    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+    ga('create','UA-XXXXX-X');ga('send','pageview');
+</script>
+
+
--- app/views/shared/_head.blade.php
+++ app/views/shared/_head.blade.php
@@ -1,25 +1,16 @@
 <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Projesom</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title>Projesom</title>
-	<meta name="description" content="">
-	<meta name="keywords" content="">
-	<meta name="robots" content="">
-
-	<!-- Configurações para o Facebook -->
-	<meta property="og:title" content="" />
-	<meta property="og:type" content="" />
-	<meta property="og:description" content="Não ultrapasse 250 caracteres"/>
-	<meta property="og:url" content="" />
-	<meta property="og:site_name" content="" />
-	<meta property="og:image " content=""/>
-	<meta property="fb:app_id" content=""/>
-
-	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-	<link rel="shortcut icon" href="favicon.ico" />
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
 	<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+	{{ HTML::style('public/css/normalize.css'); }}
+	{{ HTML::style('public/css/main.css'); }}
 	{{ HTML::style('public/css/application.css'); }}
 
+	{{ HTML::script('public/js/vendor/modernizr-2.6.2.min.js'); }}
 </head>
