ac19f3eb125fb3a1dc77fb6a395b48ed7d95cd33
Victor Volker <victor@lapisraro.com.br>
2014-03-25 21:53:18 -0300

    timeline css advanced

--- app/views/pages/_timeline.blade.php
+++ app/views/pages/_timeline.blade.php
@@ -3,4 +3,32 @@
 	<h2>Linha do tempo</h2>
 	<h3>Experiência de X anos aliada a muita pesquisa e trabalho</h3>
 
-</div:
+
+	<ul class="timeline">
+		<li class="year-left">
+			<div class="year"><span>1977</span></div>
+			<div class="description">
+				<div class="d-arrow"></div>
+				<div class="description-container">Inspirados no boom tecnológico em todo o mundo, surge a Projesom como referência no mercado de tecnologia audiovisual em Minas Gerais.
+				</div>
+			</div>
+		</li>
+		<li class="year-left">
+			<div class="year"><span>1995</span></div>
+			<div class="description">
+				<div class="d-arrow"></div>
+				<div class="description-container">Inspirados no boom tecnológico em todo o mundo, surge a Projesom como referência no mercado de tecnologia audiovisual em Minas Gerais.
+				</div>
+			</div>
+		</li>
+		<li class="year-right">
+			<div class="description">
+				<div class="description-container">Reformulação de identidade visual e participação no evento etc e formulação de identidade visual e participação no evento Tal...
+				</div>
+				<div class="d-arrow"></div>
+			</div>
+			<div class="year"><span>1995</span></div>
+		</li>
+	</ul>
+
+</div>
