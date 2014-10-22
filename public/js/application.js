5a1e50bcad4a091e71eb26ee63ed5be347fc5a17
Victor Volker <victor@lapisraro.com.br>
2014-10-22 13:56:08 -0200

    animação hover do menu; excluindo js obsoleto

--- public/js/application.js
+++ public/js/application.js
@@ -5,10 +5,51 @@ $(document).ready(function() {
 
 	initSkrollr();
 
+
+	turnMenuMouseEventsOn();
+
 });
 
 
 
 function initSkrollr() {
 	s = skrollr.init();
+}
+
+
+
+
+function turnMenuMouseEventsOn() {
+
+	$('#main-menu ul.nav li a').on('mouseenter', function() {
+		setMenuPosition($(this));
+	});
+
+	$('#main-menu ul.nav').on('mouseleave', function(){
+		setMenuPosition();
+	});	
+}
+
+function turnMenuMouseEventsOff() {
+
+	$('#main-menu ul.nav li a').off('mouseenter');
+	$('#main-menu ul.nav').off('mouseleave');
+
+}
+
+function setMenuPosition($item) {
+	$('#menu_marker').stop();
+	if (!$item) {
+		$item = $('#main-menu ul.nav li a.selected');
+	}
+	if ($item.length == 1) {
+		if ($('#menu_marker').is(":visible")) {
+			$('#menu_marker').fadeIn('fast').animate({left: $item.offset().left, width: $item.parent().width()});
+		} else {
+			$('#menu_marker').css({left: $item.offset().left, width: $item.parent().width()}).fadeIn('fast');
+		}
+	} else {
+		$('#menu_marker').fadeOut();
+	}
+
 }
--- public/js/megabanner.js
+++
@@ -1,5 +0,0 @@
-
-
-var home_img_prop = 2.8636363636363636363636363636364;
-var home_orig_height = 352;
-var home_height = 352;
