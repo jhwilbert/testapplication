bd206357d77946a15af2f2035680de6c16bc66c6
Victor <victorbv@gmail.com>
2014-10-27 20:58:39 -0200

    Contact

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -3,9 +3,10 @@
 @section('content')
 
 	@include('partials/_megabanner')
-	@include('partials/_clients')
 	@include('partials/_projects')
 	@include('partials/_solutions')
 	@include('partials/_timeline')
+	@include('partials/_clients')
+	@include('partials/_contact')
 
 @stop
--- app/views/partials/_contact.blade.php
+++ app/views/partials/_contact.blade.php
@@ -1,6 +1,18 @@
-<section class="section skrollable" id="section-contact"{{ $sd }}>
+<?php
 
-	<div class="inner-content">
+	global $skPos;
+	if (!$skPos) $skPos = 0;
+	else $skPos -= 300;
+
+	$skData  = ' data-'. $skPos      .'="top[outCubic]:100%"';
+	$skData .= ' data-'.($skPos+=400).'="top:0%"';
+	$skData .= ' data-'.($skPos+=900).'="top:0%"';
+	$skData .= ' data-'.($skPos+=400).'="top:-100%"';
+?>
+
+<section class="section skrollable" id="section-contact"{{ $skData }}>
+
+	<div class="inner-content container">
 		
 		<h2>Contato</h2>
 
@@ -13,14 +25,13 @@
 				Brasil
 				</p>
 			</div>
-			<div class="col-md-6 last">
+			<div class="col-md-6">
 				<img src="{{ asset('public/img/contact/phone_icon.png') }}">	
 				<p>(31) 32854999 </p>
 				
 				<img src="{{ asset('public/img/contact/email_icon.png') }}">	
 				<p><a href="mailto:projesom@projesom.com">projesom@projesom.com</a></p>
 			</div>
-			<div class="clearfix"></div>
 		</div>
 	</div>
 
