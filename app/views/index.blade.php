3034a9862d45a02df6013e9a0b1072a14bce0c94
Victor Volker <victor@lapisraro.com.br>
2014-10-29 14:15:35 -0200

    home back to regular sections

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -2,8 +2,9 @@
 
 @section('content')
 
-	@include('pages/solutions/_solutions')
+	@include('shared/_megabanner')
 	@include('pages/projects/_projects')
+	@include('pages/solutions/_solutions')
 	@include('pages/the_company/_timeline')
 	@include('partials/_clients')
 	@include('shared/_contact')
