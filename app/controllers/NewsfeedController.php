dbcd106c3dbb5546a30ed30222ec543988ceb6d4
Victor <victorbv@gmail.com>
2014-11-03 20:41:20 -0200

    newsfeed

--- app/controllers/NewsfeedController.php
+++ app/controllers/NewsfeedController.php
@@ -5,12 +5,21 @@ class NewsfeedController extends BaseController {
 
 	public function index() {
 
+
+		if (Route::currentRouteName() == 'projects_en') {
+			App::setLocale('en');
+			$lpr = '_en';
+		} else {
+			$lpr = '';
+		}
+
 		$megabanners = Megabanner::where('active', 1)->where('show_in_newsfeed', 1)->get();
 
 		/* make view */
 
 		return View::make('newsfeed.index', array(
 			'megabanners' => $megabanners,
+			'lpr' => $lpr
 		));		
 	}
 }
