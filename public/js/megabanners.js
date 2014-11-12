8b722006a368580441bc239c272b63fb36623b74
Victor Volker <victor@lapisraro.com.br>
2014-11-12 13:18:45 -0200

    correção em megabanners

--- public/js/megabanners.js
+++ public/js/megabanners.js
@@ -1,34 +1,40 @@
 
 $(document).ready(function() {
 
-	$('#megabanners').slidesjs({
-		width: 1008,
-		height: 352,
-		navigation: false,
-		play: {
-			active: false,
-			// [boolean] Generate the play and stop buttons.
-			// You cannot use your own buttons. Sorry.
-			effect: "slide",
-			// [string] Can be either "slide" or "fade".
-			interval: 5000,
-			// [number] Time spent on each slide in milliseconds.
-			auto: true,
-			// [boolean] Start playing the slideshow on load.
-			swap: true,
-			// [boolean] show/hide stop and play buttons
-			pauseOnHover: false,
-			// [boolean] pause a playing slideshow on hover
-			restartDelay: 2500
-			// [number] restart delay on inactive slideshow
-		}		
-	});
-
 	var l = $('#megabanners .megabanner-image').length;
-	if (l > 1) {
+
+	if ($('#megabanners .megabanner-image').length > 1) {
+
+		$('#megabanners').slidesjs({
+			width: 1008,
+			height: 352,
+			navigation: false,
+			play: {
+				active: false,
+				// [boolean] Generate the play and stop buttons.
+				// You cannot use your own buttons. Sorry.
+				effect: "slide",
+				// [string] Can be either "slide" or "fade".
+				interval: 5000,
+				// [number] Time spent on each slide in milliseconds.
+				auto: true,
+				// [boolean] Start playing the slideshow on load.
+				swap: true,
+				// [boolean] show/hide stop and play buttons
+				pauseOnHover: false,
+				// [boolean] pause a playing slideshow on hover
+				restartDelay: 2500
+				// [number] restart delay on inactive slideshow
+			}		
+		});
+
 		$('#megabanners .slidesjs-pagination').show().css('width', l * 15);
+
 	} else {
+		
+		$('#megabanners').show();
 		$('#megabanners .slidesjs-pagination').hide();
+
 	}
 
 });
