df6fd828ce1a37d2088829e4fd2b63af407e3a8b
Victor Volker <victor@lapisraro.com.br>
2014-12-09 22:02:31 -0200

    skrollr para mobile

--- public/js/application.js
+++ public/js/application.js
@@ -12,7 +12,7 @@ $(window).load(function() {
 
 function initSkrollr() {
 	s = skrollr.init({
-		skrollrBody: 'skrollr-body',
+		skrollrBody: '#main_container',
 		keyframe: function(element, name, direction) {
 			if ($(element).attr('id') == 'section-projects') getProjectsSkrollrEvents(name, direction);
 			else if ($(element).attr('id') == 'section-solutions') getSolutionsSkrollrEvents(name, direction);
