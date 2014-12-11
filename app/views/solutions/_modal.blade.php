79778c22bfe676ff3e234132ad1c7debd907d1ea
Victor Volker <victor@lapisraro.com.br>
2014-12-10 22:06:33 -0200

    ajustes css modal / mobile

--- app/views/solutions/_modal.blade.php
+++ app/views/solutions/_modal.blade.php
@@ -2,7 +2,7 @@
 	<div class="modal-dialog modal-lg">
 		<div class="modal-content">
 			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
 			</div>  	
 			<div class="modal-body">
 				<h3 id="solution-title"></h3>
