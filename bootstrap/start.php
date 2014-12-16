cac39ffc8e6f19ec4a6633ca3d626b562b4ac40e
Victor <victorbv@gmail.com>
2014-12-15 22:49:59 -0200

    other local machine

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('Victor.local'),
+	'local' => array('Victor.local', 'Victor-PC'),
 
 ));
 
