ffb292dc282110a1c3421bf0c2622183ae62e7dd
Victor Volker <victor@lapisraro.com.br>
2015-01-14 16:02:15 -0200

    Conteúdo de Empresa -> Estrutura

--- app/views/company/_structure.blade.php
+++ app/views/company/_structure.blade.php
@@ -18,18 +18,18 @@
 
 			<div class="row">
 				<div class="col-md-4">
-					<img src="{{ asset('public/img/company/estrutura_img1.jpg') }}">
+					<img src="{{ asset('public/img/company/IMG_20150113_172930.jpg') }}">
 				</div>
 				<div class="col-md-8">
 					@if (App::getLocale() == 'en')
-						<h3>Our Office</h3>	
+						<h3>Tradição sempre atualizada</h3>	
 						<p class="text">
-							Falar do acervo e estrutura do escritório, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+							Aliar o pioneirismo à vanguarda é o nosso desafio de todo dia. Colocamos à disposição de nossos clientes décadas de experiência somadas ao que há de mais moderno em audiovisual. 
 						</p>
 					@else
-						<h3>Nossa Sede</h3>	
+						<h3>Tradição sempre atualizada</h3>	
 						<p class="text">
-							Falar do acervo e estrutura do escritório, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+							Aliar o pioneirismo à vanguarda é o nosso desafio de todo dia. Colocamos à disposição de nossos clientes décadas de experiência somadas ao que há de mais moderno em audiovisual. 
 						</p>
 					@endif
 				</div>
@@ -38,40 +38,22 @@
 			<div class="row">
 				<div class="col-md-8">
 					@if (App::getLocale() == 'en')
-						<h3>Our team</h3>	
+						<h3>Experiência internacional</h3>	
 						<p class="text">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+							A Projesom está presente nas maiores feiras e congressos de audiovisual em todo o mundo. Manter os olhos no futuro é fundamental para ficar por dentro de tudo que está por vir em uma área que evolui tão rapidamente.
 						</p>
 					@else
-						<h3>Equipe</h3>	
+						<h3>Experiência internacional</h3>	
 						<p class="text">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+							A Projesom está presente nas maiores feiras e congressos de audiovisual em todo o mundo. Manter os olhos no futuro é fundamental para ficar por dentro de tudo que está por vir em uma área que evolui tão rapidamente.
 						</p>
 					@endif					
 				</div>
 				<div class="col-md-4">
-					<img src="{{ asset('public/img/company/estrutura_equipe.jpg') }}">
+					<img src="{{ asset('public/img/company/IMG_20150113_172731.jpg') }}">
 				</div>
 			</div>
-			
-			<div class="row">
-				<div class="col-md-4">
-					<img src="{{ asset('public/img/company/estrutura_diretoria.jpg') }}">
-				</div>
-				<div class="col-md-8">
-					@if (App::getLocale() == 'en')
-						<h3>Diretoria</h3>	
-						<p class="text">
-							O casal João Carlos e Georgina Wilbert são os gestores da Projesom. Experiência, curiosidade, bom senso e honestidade são marcas e valores pessoais da dupla. Essas características são também valorizadas na política de administração da empresa.
-						</p>
-					@else
-						<h3>Diretoria</h3>	
-						<p class="text">
-							O casal João Carlos e Georgina Wilbert são os gestores da Projesom. Experiência, curiosidade, bom senso e honestidade são marcas e valores pessoais da dupla. Essas características são também valorizadas na política de administração da empresa.
-						</p>
-					@endif
-				</div>
-			</div>
+
 
 		</div>
 
