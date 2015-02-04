b1432283ee9017a85fc9b2d598b9059b1d80febc
Victor Volker <victor@lapisraro.com.br>
2015-02-04 14:18:38 -0200

    projects modal adjustments

--- app/views/projects/_modal.blade.php
+++ app/views/projects/_modal.blade.php
@@ -1,9 +1,9 @@
-<div class="modal fade gallery-modal" id="show-project">
+<div class="modal fade gallery-modal" id="show-project" tabindex="-1" role="dialog" aria-hidden="true">
 	<div class="modal-dialog modal-lg">
 		<div class="modal-content">
 			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-			</div> 
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			</div>
 			<div class="modal-body">
 				<div id="project-slides" class="slides"></div>
 				<h3 id="project-title"></h3>
@@ -12,6 +12,6 @@
 					<div id="project-technology" class="col-md-6"></div>
 				</div>
 			</div>
-		</div><!-- /.modal-content -->
-	</div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
+		</div>
+	</div>
+</div>
